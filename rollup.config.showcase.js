ce24117ee7bb3e8cea48930a28f9495e9eeab1a1
Christian M <chriamue@gmail.com>
2021-11-08 14:24:14 +0100

    added default urls

--- rollup.config.showcase.js
+++ rollup.config.showcase.js
@@ -7,16 +7,16 @@ import svelte from 'rollup-plugin-svelte';
 
 const production = !process.env.ROLLUP_WATCH;
 
-const defaultRedirectUri = production ? 'https://darrelopry.com/svelte-oidc' : 'http://localhost:5000/';
-const defaultPostLogoutRedirectUri = production ? 'https://darrelopry.com/svelte-oidc' : 'http://localhost:5000/';
+const defaultRedirectUri = production ? 'https://nft-login.github.io/svelte-oidc' : 'http://localhost:5000/';
+const defaultPostLogoutRedirectUri = production ? 'https://nft-login.github.io/svelte-oidc' : 'http://localhost:5000/';
 
 export default {
 	input: 'src/main.js',
 	output:  { sourcemap: true,	format: 'iife',	name: 'app', file: 'public/bundle.js'  },
 	plugins: [
 		replace({
-			'process.env.OIDC_ISSUER': process.env.OIDC_ISSUER || "https://dev-hvw40i79.auth0.com",
-			'process.env.OIDC_CLIENT_ID': process.env.OIDC_CLIENT_ID || "5m4i3ZD9M3NqX4qQsB0nsBmCXb6OXBN2",
+			'process.env.OIDC_ISSUER': process.env.OIDC_ISSUER || "https://nft-login.chriamue.net",
+			'process.env.OIDC_CLIENT_ID': process.env.OIDC_CLIENT_ID || "0x420d2a6E87D87992EB01e5BFe762B3F437dBfD85",
 			'process.env.OIDC_REDIRECT_URI': process.env.OIDC_REDIRECT_URI ||  defaultRedirectUri,
 			'process.env.OIDC_POST_LOGOUT_REDIRECT_URI': process.env.OIDC_POST_LOGOUT_REDIRECT_URI ||  defaultPostLogoutRedirectUri,
 			'pkg.version': pkg.version
