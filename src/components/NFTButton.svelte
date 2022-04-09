1fd575d25a12e60a35010514ba12e706fe0a05bf
Christian M <7541795+chriamue@users.noreply.github.com>
2022-04-09 19:47:27 +0200

    fix marketplace url

--- src/components/NFTButton.svelte
+++ src/components/NFTButton.svelte
@@ -1,5 +1,5 @@
 <script>
-	let marketplace = 'https://web3-login.net/nft-market';
+	let marketplace = 'https://web3-login.net/nft-marketplace/';
 	const open = () => {
 		window.open(marketplace, '_blank');
 	};
